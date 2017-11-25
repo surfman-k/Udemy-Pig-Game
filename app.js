a801d2a140bc076a6e16bacd7088ea8a678ed255
Konrad Kipczak <konrad.kipczak@gmail.com>
2017-11-25 14:19:58 -0500

    Fixed bug in final score, added input field for user to set desired final score.

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
 		//check if player won game
-		if (scores[activePlayer] >= 100 {
+		if (scores[activePlayer] >= 100) {
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
